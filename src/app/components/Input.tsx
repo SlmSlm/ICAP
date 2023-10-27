2b16c41c96a6971b70c00d99634f6356b512c157
SlmSlm <solomonkoviktor67@gmail.com>
2023-10-27 14:01:30 +0300

    Input border bug fixed

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -27,7 +27,7 @@ const Input: React.FC<IInputProps> = ({
         autoComplete={target}
         required
         value={value}
-        className="block min-w-max w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        className="block min-w-max w-full rounded-md border-2 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         onChange={(e) => handleChange(target, e.target.value, rowIndex)}
       />
 
